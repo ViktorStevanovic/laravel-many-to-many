be14abaa06664ebbd5c804191d7c0811da4e8c9d
ViktorStevanovic <stevanovic.lavoro@gmail.com>
2024-02-15 14:02:40 +0100

    implemented relation in both models

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             TypeSeeder::class,
             ProjectSeeder::class,
+            TechnologySeeder::class,
 
         ]);
     }
